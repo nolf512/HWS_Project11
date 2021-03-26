a8265835ada19ed5df94c7cf0788f5c002a89b70
nofprog <k.jin3217@gmail.com>
2021-03-26 23:57:24 +0900

    backgroundの追加

--- HWS_Project11/GameScene.swift
+++ HWS_Project11/GameScene.swift
@@ -12,6 +12,9 @@ class GameScene: SKScene {
  
     override func didMove(to view: SKView) {
         
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        addChild(background)
         
         
     }
